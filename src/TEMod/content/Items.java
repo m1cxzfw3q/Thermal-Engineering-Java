8846ba4fdc55d74e52f83a1a0129d38b6587aeaf
mcxzfwq <mcxzfwq@outlook.com>
2025-04-09 20:20:26 +0800

    test

--- src/TEMod/content/Items.java
+++ src/TEMod/content/Items.java
@@ -4,7 +4,8 @@ import mindustry.type.Item;
 import arc.graphics.Color;
 
 public class Items {
-    public static Item uranium,nuclear_Fuel_Rod;
+    public static Item uranium;
+    public static Item nuclear_Fuel_Rod;
 
     public static void load() {
         uranium = new Item("Uranium", Color.valueOf("d99d73")){{
