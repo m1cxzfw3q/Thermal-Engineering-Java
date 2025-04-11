d23350e28ddd08d67e7db01a671a4c524bbf276f
mcxzfwq <mcxzfwq@outlook.com>
2025-04-12 01:51:25 +0800

    加史

--- src/TEMod/content/KepplerPlanet.java
+++ src/TEMod/content/KepplerPlanet.java
@@ -1,7 +1,46 @@
 package TEMod.content;
 
-public class KepplerPlanet {
-    public static void load() {
+import arc.graphics.Color;
+import mindustry.game.Team;
+import mindustry.graphics.Pal;
+import mindustry.graphics.g3d.HexMesh;
+import mindustry.graphics.g3d.HexSkyMesh;
+import mindustry.graphics.g3d.MultiMesh;
+import mindustry.maps.planet.SerpuloPlanetGenerator;
+import mindustry.type.Planet;
 
+public class KepplerPlanet {
+    public static Planet keppler;
+    public static void load() {
+        keppler = new Planet("keppler", null, 3) {{
+            generator = new SerpuloPlanetGenerator();
+            meshLoader = () -> new HexMesh(this, 6);
+            cloudMeshLoader = () -> new MultiMesh(
+                    new HexSkyMesh(this, 11, 0.15f, 0.13f, 5, new Color().set(Pal.spore).mul(0.9f).a(0.75f), 2, 0.45f, 0.9f, 0.38f),
+                    new HexSkyMesh(this, 1, 0.6f, 0.16f, 5, Color.white.cpy().lerp(Pal.spore, 0.55f).a(0.75f), 2, 0.45f, 1f, 0.41f)
+            );
+
+            launchCapacityMultiplier = 0.5f;
+            sectorSeed = 3;
+            allowWaves = true;
+            allowWaveSimulation = true;
+            allowSectorInvasion = true;
+            allowLaunchSchematics = true;
+            enemyCoreSpawnReplace = true;
+            allowLaunchLoadout = true;
+            prebuildBase = false;
+            ruleSetter = r -> {
+                r.waveTeam = Team.blue;
+                r.placeRangeCheck = false;
+                r.showSpawns = false;
+            };
+            iconColor = Color.valueOf("7d4dff");
+            atmosphereColor = Color.valueOf("3c1b8f");
+            atmosphereRadIn = 0.02f;
+            atmosphereRadOut = 0.3f;
+            startSector = 15;
+            alwaysUnlocked = true;
+            landCloudColor = Pal.spore.cpy().a(0.5f);
+        }};
     }
 }
