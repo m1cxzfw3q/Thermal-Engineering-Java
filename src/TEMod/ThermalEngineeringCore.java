5eff5ea5a77790f544c700eac29929bda44ed6b1
m1cxzfw3q <mcxzfwq@outlook.com>
2025-07-16 00:03:18 +0800

    test

--- src/TEMod/ThermalEngineeringCore.java
+++ src/TEMod/ThermalEngineeringCore.java
@@ -2,23 +2,12 @@ package TEMod;
 
 import TEMod.content.*;
 import TEMod.content.Keppler.*;
-import arc.Core;
-import arc.Events;
 import arc.util.Log;
-import arc.util.Time;
 import mindustry.mod.Mod;
-import mindustry.game.EventType;
-import mindustry.ui.dialogs.BaseDialog;
 
 public class ThermalEngineeringCore extends Mod {
     public ThermalEngineeringCore() {
-        Events.on(EventType.ClientLoadEvent.class, e-> Time.run(10F, () -> {
-            BaseDialog dialog = new BaseDialog("Welcome to use ThermalEngineering");
-            dialog.cont.image(Core.atlas.find("ModLogo")).pad(20f).row();
-            dialog.cont.add("test").row();
-            dialog.addCloseButton();
-            dialog.show();
-        }));
+
     }
     @Override
     public void loadContent() {
