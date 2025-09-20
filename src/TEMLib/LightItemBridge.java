289b4a87dfa21bd30dda8a08013d4a0d2d439e92
m1cxzfw3q <mcxzfwq@outlook.com>
2025-09-20 12:20:53 +0800

    test

--- src/TEMLib/LightItemBridge.java
+++ src/TEMLib/LightItemBridge.java
@@ -1,7 +1,5 @@
 package TEMLib;
 
-import mindustry.gen.Building;
-import mindustry.type.Item;
 import mindustry.world.blocks.distribution.ItemBridge;
 
 public class LightItemBridge extends ItemBridge {
@@ -9,22 +7,9 @@ public class LightItemBridge extends ItemBridge {
         super(name);
     }
     public class LightItemBridgeBuild extends ItemBridgeBuild {
-        public void updateTransport(Building other){
-            transportCounter += edelta();
-            while (transportCounter >= transportTime) {
-                output(other);
-                transportCounter -= transportTime;
-            }
-        }
-        public void output(Building other) {
-            Item item = items.take();
-            if (item != null && other.acceptItem(this, item)) {
-                other.handleItem(this, item);
-                moved = true;
-            } else if(item != null) {
-                items.add(item, 1);
-                items.undoFlow(item);
-            }
+        @Override
+        public void updateTile(){
+            while (true) super.updateTile();
         }
     }
 }
