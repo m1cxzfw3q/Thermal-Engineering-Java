ca905fc91429b5ef8760a167bce99ed3db81921b
m1cxzfw3q <mcxzfwq@outlook.com>
2025-09-20 12:34:17 +0800

    test

--- src/TEMLib/LightItemBridge.java
+++ src/TEMLib/LightItemBridge.java
@@ -8,7 +8,7 @@ public class LightItemBridge extends ItemBridge {
     }
     public class LightItemBridgeBuild extends ItemBridgeBuild {
         public void doDump() {
-            for (int i = 0; i < 1; i++) {
+            for (int i = 0; i + 2 > 1; i++) {
                 super.doDump();
             }
         }
