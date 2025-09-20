440d2a91da6b6e8c903f0227539664cdb848db54
m1cxzfw3q <mcxzfwq@outlook.com>
2025-09-20 11:59:35 +0800

    test

--- src/TEMLib/LightItemBridge.java
+++ src/TEMLib/LightItemBridge.java
@@ -11,12 +11,12 @@ public class LightItemBridge extends ItemBridge {
     public class LightItemBridgeBuild extends ItemBridgeBuild {
         public void updateTransport(Building other){
             transportCounter += edelta();
-            while(transportCounter >= transportTime) {
+            while (transportCounter >= transportTime) {
                 Item item = items.take();
-                if(item != null && other.acceptItem(this, item)){
+                if (item != null && other.acceptItem(this, item)) {
                     other.handleItem(this, item);
                     moved = true;
-                }else if(item != null){
+                } else if(item != null) {
                     items.add(item, 1);
                     items.undoFlow(item);
                 }
@@ -25,15 +25,13 @@ public class LightItemBridge extends ItemBridge {
             }
         }
         public void output(Building other) {
-            while(transportCounter >= transportTime) {
-                Item item = items.take();
-                if(item != null && other.acceptItem(this, item)){
-                    other.handleItem(this, item);
-                    moved = true;
-                }else if(item != null){
-                    items.add(item, 1);
-                    items.undoFlow(item);
-                }
+            Item item = items.take();
+            if (item != null && other.acceptItem(this, item)) {
+                other.handleItem(this, item);
+                moved = true;
+            } else if(item != null) {
+                items.add(item, 1);
+                items.undoFlow(item);
             }
         }
     }
