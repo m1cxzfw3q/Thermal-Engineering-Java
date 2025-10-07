f33520f7d03fcafcb7b6b1d35e3ae6473f37b8af
m1cxzfw3q <mcxzfwq@outlook.com>
2025-10-08 01:55:22 +0800

    test

--- src/TEMLib/PayloadLauncher.java
+++ src/TEMLib/PayloadLauncher.java
@@ -1,15 +1,8 @@
 package TEMLib;
 
-import arc.struct.ObjectMap;
-import arc.struct.OrderedMap;
-import mindustry.entities.bullet.BulletType;
-import mindustry.world.Block;
-import mindustry.world.blocks.defense.turrets.Turret;
 import mindustry.world.blocks.payloads.PayloadBlock;
 import mindustry.world.meta.BlockGroup;
 import mindustry.world.meta.Stat;
-import mindustry.world.meta.StatValues;
-import org.jetbrains.annotations.NotNull;
 
 public class PayloadLauncher extends PayloadBlock {
     public PayloadLauncher(String name) {
