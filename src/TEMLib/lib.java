0bd1d0a11b24bbcaab39481b586b7d9f2b603c1f
m1cxzfw3q <mcxzfwq@outlook.com>
2025-10-19 01:03:45 +0800

    test

--- src/TEMLib/lib.java
+++ src/TEMLib/lib.java
@@ -9,11 +9,4 @@ public class lib {//没什么用的lib
     public static ItemStack[] sizeWith(ItemStack[] stacks, Block source) {
         return ItemStack.mult(stacks, (float) Math.pow(source.size, 2));
     }
-
-    public static ItemStack[] newWith(ItemStack[] origin, ItemStack[] addItems) {
-        ItemStack[] out = new ItemStack[origin.length + addItems.length + 1];
-        System.arraycopy(origin, 0, out, 0, origin.length);
-        System.arraycopy(addItems, 0, out, origin.length + 1, addItems.length);
-        return out;
-    }
 }
